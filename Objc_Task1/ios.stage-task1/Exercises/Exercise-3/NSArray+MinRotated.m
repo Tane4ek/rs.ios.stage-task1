e7c557053045521f8545c0614e2d948379925c56
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-05-09 21:18:21 +0300

    docs: add solution of exercise 3

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,6 +3,34 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
+    if (self.count == 0) {
+        return nil;
+    }
+    if (self.count == 1) {
+        return self[0];
+    }
+    NSInteger start = 0;
+    NSInteger end = self.count - 1;
+    NSNumber *startVal = self[start];
+    NSNumber *endVal = self[end];
+    if (startVal.integerValue < endVal.integerValue) {
+        return startVal;
+    }
+    for (;;) {
+        NSNumber *startVal = self[start];
+        NSNumber *endVal = self[end];
+        if ( end == start + 1) {
+            return (endVal.integerValue < startVal.integerValue ? endVal : startVal);
+        }
+        NSInteger mid = (start + end) / 2;
+        NSNumber *midVal = self[mid];
+        if (midVal.integerValue < startVal.integerValue ) {
+            mid = end;
+        }else {
+            start = end;
+        }
+    }
+    
     return nil;
 }
 
