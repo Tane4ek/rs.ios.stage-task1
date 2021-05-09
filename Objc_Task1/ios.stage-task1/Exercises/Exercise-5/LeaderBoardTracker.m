ff13d1370f930e13344038840244d3159719ca83
Tatyana Luzanova <82838416+Tane4ek@users.noreply.github.com>
2021-05-09 22:42:43 +0300

    docs: add solution of exercise 5

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,33 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    NSMutableArray *results = [[NSMutableArray alloc] init];
+    for (NSNumber *player in playerArray) {
+        if (rankedArray.count == 0) {
+            [results addObject: [NSNumber numberWithInteger:1]];
+        }else{
+            NSInteger score = 0;
+            NSInteger prevScore = -1;
+            NSInteger result = -1;
+            for (NSNumber *ranked in rankedArray) {
+                if (player.integerValue >= ranked.integerValue){
+                    result = score + 1;
+                    break;
+                }
+                if (prevScore != ranked.integerValue){
+                    prevScore = ranked.integerValue;
+                    score +=1;
+                }
+            }
+            if (result == -1){
+                result = score + 1;
+            }
+            [results addObject: [NSNumber numberWithInteger:result]];
+        }
+    }
+    
+
+    return results;
 }
 
 @end
